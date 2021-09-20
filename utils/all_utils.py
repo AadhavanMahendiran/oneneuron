a0c9b0b7564a1f5e5343afcd8c71bce8fd1e1cb3
AadhavanMahendiran <aadhavan.svm@gmail.com>
2021-09-20 19:19:45 +0530

    Commit message.

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -9,6 +9,14 @@ plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
 
 
 def prepare_data(df):
+  """it's used to seperate the dependent and independent features
+
+  Args:
+      df (pd.DataFrame): it's the pandas DataFrame to
+
+  Returns:
+      tuple: it return's the tuple of dependent variable and independent variable
+  """
   X = df.drop("y", axis=1)
 
   y = df["y"]
@@ -16,6 +24,13 @@ def prepare_data(df):
   return X, y
 
 def save_model(model, filename):
+  """This saves the trained model to      
+
+  Args:
+      model (python object): trained model to
+      filename (str): path to save the trained model
+  """
+
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
@@ -23,6 +38,11 @@ def save_model(model, filename):
 
 
 def save_plot(df, file_name, model):
+  """ 
+  :param df:its a dataframe
+  :param filename:its path to save the plot
+  :param model:trained model
+  """
   def _create_base_plot(df):
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
